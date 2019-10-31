d5ecef89b155488eaaf1536f00bba39ed73c2e7f
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-10-31 17:17:51 -0400

    Misc. tweaks

--- src/player.py
+++ src/player.py
@@ -1,10 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room, list=[]):
+    def __init__(self, name, current_room, list=None):
         self.name = name
         self.current_room = current_room
-        self.list = list
+        self.list = [] if list is None else list
     def __str__(self):
         return f"{self.name}: {self.list}"
     def __repr__(self):
